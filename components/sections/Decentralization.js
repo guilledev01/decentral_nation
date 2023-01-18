a1dd6715aa21869d6b2a0a248ff3a571666b8b0b
Guillermo <guille1000142@gmail.com>
2023-01-18 19:59:04 +0100

    eslint fixed

--- components/sections/Decentralization.js
+++ components/sections/Decentralization.js
@@ -12,20 +12,36 @@ const DECENTRALIZATION = {
   introduction:
     "Decentralization is the process of distributing power and control away from centralized entities. In the digital world, this means creating systems and applications that are not controlled by a single entity, but rather by a network of users. This allows for a more secure and transparent system, as there is no single point of failure.",
   blockchain: [
-    "One of the key elements of decentralization is the use of blockchain technology. A blockchain is a digital ledger that is used to record and verify transactions, create smart contracts, and more.",
-    "We offer development on different blockchain platforms such as Ethereum, Polygon, BSC and many more. This allows you to choose the best platform for your project, based on your specific needs and requirements.",
-    "By developing your decentralized application on a blockchain platform, you can be sure that your data will be safe, secure and easily accessible to all parties involved.",
+    {
+      icon: <BlockchainIcon />,
+      text: "One of the key elements of decentralization is the use of blockchain technology. A blockchain is a digital ledger that is used to record and verify transactions, create smart contracts, and more.",
+    },
+    {
+      icon: <NetworkIcon />,
+      text: "We offer development on different blockchain platforms such as Ethereum, Polygon, BSC and many more. This allows you to choose the best platform for your project, based on your specific needs and requirements.",
+    },
+    {
+      icon: <ShieldIcon />,
+      text: "By developing your decentralized application on a blockchain platform, you can be sure that your data will be safe, secure and easily accessible to all parties involved.",
+    },
   ],
   ipfs: [
-    "We specialize in using IPFS (InterPlanetary File System) as part of our decentralized solutions. IPFS is a decentralized protocol that allows for the storage and retrieval of files on a distributed network.",
-    "This is particularly useful for decentralized applications, as it allows for the storage of large amounts of data, such as media files, metadata for NFTs and create a decentralized database",
-    "It allows faster data retrieval and lower latency due to peer to peer nodes. IPFS can provide a secure and efficient solution for your data.",
+    {
+      icon: <IpfsIcon />,
+      text: "We specialize in using IPFS (InterPlanetary File System) as part of our decentralized solutions. IPFS is a decentralized protocol that allows for the storage and retrieval of files on a distributed network.",
+    },
+    {
+      icon: <DatabaseIcon />,
+      text: "This is particularly useful for decentralized applications, as it allows for the storage of large amounts of data, such as media files, metadata for NFTs and create a decentralized database",
+    },
+    {
+      icon: <NodeIcon />,
+      text: "It allows faster data retrieval and lower latency due to peer to peer nodes. IPFS can provide a secure and efficient solution for your data.",
+    },
   ],
   conclusion:
     "By using IPFS in conjunction with blockchain technology, we can provide a truly decentralized solution that is both secure and efficient. This allows you to take advantage of the strengths of both technologies, and create a robust and reliable decentralized application.",
 };
-const BLOCKCHAIN_LOGO = [<BlockchainIcon />, <NetworkIcon />, <ShieldIcon />];
-const IPFS_LOGO = [<IpfsIcon />, <DatabaseIcon />, <NodeIcon />];
 
 export default function Decentralization({ isMobileResolution }) {
   return (
@@ -41,7 +57,7 @@ export default function Decentralization({ isMobileResolution }) {
           <div className="d-flex col ai-c jc-c gp-64">
             <h4>Blockchain</h4>
             <div className="d-flex flex-wrap ai-c jc-se gp-64">
-              {DECENTRALIZATION.blockchain.map((text, id) => {
+              {DECENTRALIZATION.blockchain.map(({ icon, text }, id) => {
                 return (
                   <ZoomEffect key={id} top delay={parseInt(`${id + 1 * 5}00`)}>
                     <div
@@ -52,9 +68,7 @@ export default function Decentralization({ isMobileResolution }) {
                       }}
                     >
                       <div className="rombo1 d-flex ai-c jc-c">
-                        <div className="rombo2 d-flex ai-c jc-c">
-                          {BLOCKCHAIN_LOGO[id]}
-                        </div>
+                        <div className="rombo2 d-flex ai-c jc-c">{icon}</div>
                       </div>
                       <span>{text}</span>
                     </div>
@@ -68,7 +82,7 @@ export default function Decentralization({ isMobileResolution }) {
           <div className="d-flex col ai-c jc-c gp-64">
             <h4>IPFS</h4>
             <div className="d-flex flex-wrap ai-c jc-se gp-64">
-              {DECENTRALIZATION.ipfs.map((text, id) => {
+              {DECENTRALIZATION.ipfs.map(({ icon, text }, id) => {
                 return (
                   <ZoomEffect
                     key={id}
@@ -84,9 +98,7 @@ export default function Decentralization({ isMobileResolution }) {
                       }}
                     >
                       <div className="rombo1 d-flex ai-c jc-c">
-                        <div className="rombo2 d-flex ai-c jc-c">
-                          {IPFS_LOGO[id]}
-                        </div>
+                        <div className="rombo2 d-flex ai-c jc-c">{icon}</div>
                       </div>
                       <span>{text}</span>
                     </div>
