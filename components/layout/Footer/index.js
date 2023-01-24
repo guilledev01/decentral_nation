9c21304007d33e34a9e51e2f3a0b04fa44e89703
Guillermo <guille1000142@gmail.com>
2023-01-24 13:18:20 +0100

    all lang fixed

--- components/layout/Footer/index.js
+++ components/layout/Footer/index.js
@@ -1,26 +1,24 @@
+import useTranslation from "next-translate/useTranslation";
 import Link from "next/link";
 import { Logo } from "../../elements";
 
-const ROUTES = [
-  { title: "Request Budget", route: "/request-budget" },
-  { title: "Contact Us", route: "/contact-us" },
-  { title: "Privacy Policy", route: "/privacy-policy" },
-  { title: "Legal Warning", route: "/legal-warning" },
-  { title: "Cookies Policy", route: "/cookies-policy" },
-];
-
 export default function Footer() {
+  const { t } = useTranslation("common");
+  const path = t("path", {}, { returnObjects: true });
+
   return (
     <footer className="d-flex col ai-c jc-c gp-32">
       <Logo />
       <div className="d-flex flex-wrap ai-c jc-se gp-16 pl-32 pr-32">
-        {ROUTES.map(({ title, route }, id) => {
-          return (
-            <Link className="routes" key={id} href={route}>
-              {title}
-            </Link>
-          );
-        })}
+        {path
+          .filter((path) => path.route)
+          .map(({ title, route }, id) => {
+            return (
+              <Link className="routes" key={id} href={route}>
+                {title}
+              </Link>
+            );
+          })}
       </div>
     </footer>
   );
